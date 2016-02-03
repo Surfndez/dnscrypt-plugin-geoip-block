c1fec4c7418c9a3fbadc43d0626798fab8a6bc7f
Frank Denis <github@pureftpd.org>
2016-02-03 12:11:42 +0100

    Check ldns_wire2pkt() return code

--- geoip-block.c
+++ geoip-block.c
@@ -232,9 +232,8 @@ dcplugin_sync_post_filter(DCPlugin *dcplugin, DCPluginDNSPacket *dcp_packet)
     if (context->blacklist == NULL) {
         return DCP_SYNC_FILTER_RESULT_OK;
     }
-    ldns_wire2pkt(&packet, dcplugin_get_wire_data(dcp_packet),
-                  dcplugin_get_wire_data_len(dcp_packet));
-    if (packet == NULL) {
+    if (ldns_wire2pkt(&packet, dcplugin_get_wire_data(dcp_packet),
+                      dcplugin_get_wire_data_len(dcp_packet)) != LDNS_STATUS_OK) {
         return DCP_SYNC_FILTER_RESULT_ERROR;
     }
     if ((result = apply_block_ips(dcp_packet, context, packet)
